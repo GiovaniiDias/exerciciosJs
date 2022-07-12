bb881543973b9e68c48604cfa3063de47e403fc3
GiovaniiDias <giovani.eb3@gmail.com>
2022-07-12 15:51:57 -0300

    add todos ex no github comitado para github

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 50
+console.log("A velocidade do seu carro é "  +vel+ " km/h")
+
+if (vel>60) {  //condição simples
+    console.log("vc ultrapassou a velocidade permitida")
+}
+
+console.log("console funcionou!")
+
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = "cuba"
+console.log("vivendo em " +pais)
+if (pais == "brasil") {
+    console.log("vc é brasileiro!")
+} else{
+    console.log("vc é de fora!")
+}
